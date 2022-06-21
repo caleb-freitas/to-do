d78d7a2cf9a8ecd70f30c9c6a33280dda1ef635d
caleb-freitas <calebfreitas@tutanota.com>
2022-06-21 13:08:02 -0300

    feat: added TasksInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './global.css'
 import { Header } from './components/Header'
 import { Input } from './components/Input'
+import { TasksInfo } from './components/TasksInfo'
 
 export function App() {
   return (
     <>
       <Header />
       <Input />
+      <TasksInfo />
     </>
   )
 }
---
+++ src/components/TasksInfo.tsx
@@ -0,0 +1,16 @@
+import styles from './Task.module.css'
+
+export function TasksInfo() {
+  return (
+    <header className={styles.tasks}>
+      <div className={styles.created}>
+        <p className={styles.createdTasks}>Created Tasks</p>
+        <p className={styles.counter}>10</p>
+      </div>
+      <div className={styles.finished}>
+        <p className={styles.finishedTasks}>Finished</p>
+        <p className={styles.counter}>5 de 10</p>
+      </div>
+    </header>
+  )
+}
